aba41337e8871f38cd6e2d401a9f43be7d60b437
ivanjancic <ijancic@gmail.com>
2015-09-05 12:37:13 +0200

    Update PokerPlayer.cs

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -5,23 +5,30 @@ namespace Nancy.Simple
 {
 	public static class PokerPlayer
 	{
-		public static readonly string VERSION = "Default C# folding player";
+
+		public static readonly string VERSION = "Dolphins player";
 
 		public static int BetRequest(JObject gameState)
 		{
-			//TODO: Use this method to return the value You want to bet
-			Console.Out.WriteLine(gameState["players"].ToString());
-			foreach (JObject ply in gameState["players"])
-            		{
-        			 Console.Out.WriteLine(ply["name"]);
-            		}
-			return 150;
+            //TODO: Use this method to return the value You want to bet
+            Console.Out.WriteLine(gameState.ToString());
+
+            int maxbet=0;
+
+            foreach (JObject ply in gameState["players"])
+            {
+                Console.Out.WriteLine(ply["name"]);
+                int curbet = int.Parse(ply["bet"].ToString());
+                if (curbet > maxbet) maxbet = curbet;
+            }
+			return maxbet+10;
 		}
 
 		public static void ShowDown(JObject gameState)
 		{
 			//TODO: Use this method to showdown
 		}
+
 	}
 }
 
